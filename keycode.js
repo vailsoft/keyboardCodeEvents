db738f489ace89328983da255d1f40b0346dfc6b
vails <vailsoft@hotmail.com>
2021-09-27 08:25:12 -0300

    Ficou mais bonito

--- keycode.js
+++ keycode.js
@@ -1,6 +1,8 @@
-var el = document.getElementById("result");
+var table = document.querySelector("#result");
 function minhaFuncao(codigo,tecla) {
-    el.innerHTML += "[Key: "+ tecla + " | Code: "+ codigo+"]<br>";    
+    let tr = document.createElement('tr');
+    tr.innerHTML += "<tr><td>"+ tecla + "</td><td>"+ codigo+"</td></tr><br>";    
+    table.appendChild(tr);
 }
 
 document.addEventListener('keydown', function(e) {
